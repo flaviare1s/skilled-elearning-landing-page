d8fd99e502287baaff0f790e8a1183a6dc555235
Flávia Reis <flaviarster@gmail.com>
2024-03-18 16:36:21 -0300

    I added the font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'Jakarta': ['Plus Jakarta Sans', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
